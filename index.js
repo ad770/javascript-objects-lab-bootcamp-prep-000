8efa5ca49918f726c4c309cc417f9540571c4383
arye dov eidelman <aryedov.e@gmail.com>
2018-02-01 20:01:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
-  return Object;
+  return object;
 }
 function deleteFromObjectByKey(object, key){
 
