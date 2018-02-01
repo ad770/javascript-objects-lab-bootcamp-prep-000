59f9a8efff5fe80dec18c9d70bbea45167c155e1
arye dov eidelman <aryedov.e@gmail.com>
2018-02-01 20:04:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,14 +17,3 @@ function destructivelyDeleteFromObjectByKey(object, key){
 }
 
 
-
-// playlist = { Kanye: "Gold Digger" };
-//
-// function updatePlaylist(playlist, artistName, songTitle){
-//   return Object.assign({}, playlist, {[artistName]: songTitle});
-// }
-// function removeFromPlaylist(playlist, artistName){
-//   temp = Object.assign({}, playlist);
-//  delete temp[artistName];
-//  return temp;
-// }
